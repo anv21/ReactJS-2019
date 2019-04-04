f345ce4494316baeaf7db727f1ae83c6648ed1bc
Anastasiya <novakanastasya@yandex.ru>
2019-04-04 09:09:27 +0300

    Task4: Testing lecture

--- src/components/Items/index.jsx
+++ src/components/Items/index.jsx
@@ -2,33 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Item from '../Item/index.jsx';
 
-class Items extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const {items, onItemClick} = this.props;
-
-        return (
-            <div className="items_section">
-                <div className="items_wrapper">
-                    {items.length ? (
-                        items.map(item => (
-                            <Item
-                                key={item.id}
-                                item={item}
-                                onItemClick={onItemClick}
-                            />
-                        ))
-                    ) : (
-                        <div className="items_empty_result">No films found</div>
+const Items = (props) => (
+    <div className="items_section">
+        <div className="items_wrapper">
+            {props.items.length ? (
+                props.items.map(item => (
+                    <Item
+                        key={item.id}
+                        item={item}
+                        onItemClick={props.onItemClick}
+                    />
+                ))
+                ) : (
+                    <div className="items_empty_result">No films found</div>
                     )}
-                </div>
-            </div>
-        )
-    }
-}
+        </div>
+    </div>
+);
 
 Items.propTypes = {
     items: PropTypes.array,
