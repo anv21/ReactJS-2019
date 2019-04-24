7798fb44548e0dd9e53577a4703cbc6a8aed90e4
anv21 <novakanastasya@yandex.ru>
2019-04-24 23:33:19 +0300

    Homework-3: update server

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+export default {
     entry: {
         app: './src/index.js'
     },
